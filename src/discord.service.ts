88389b0968109850b4251607e450b528d97d3971
Maximilian Temeschinko <maxiteme@peoplesmarkets.com>
2024-02-18 20:34:58 +0100

    add inventionAttempt topic

--- src/discord.service.ts
+++ src/discord.service.ts
@@ -4,15 +4,19 @@ import { EventTopics, ForgeEvent } from './requests/ForgeEvent';
 
 @Injectable()
 export class DiscordService {
-  forgingClient: WebhookClient;
   inventingClient: WebhookClient;
+  inventionAttemptsClient: WebhookClient;
+  forgingClient: WebhookClient;
 
   constructor() {
-    this.forgingClient = new WebhookClient({
-      url: process.env.DISCORD_FORGING_CHANNEL_WEBHOOK_URL,
-    });
     this.inventingClient = new WebhookClient({
       url: process.env.DISCORD_INVENTING_CHANNEL_WEBHOOK_URL,
+    });
+    this.inventionAttemptsClient = new WebhookClient({
+      url: process.env.DISCORD_INVENTION_ATTEMPTS_CHANNEL_WEBHOOK_URL,
+    });
+    this.forgingClient = new WebhookClient({
+      url: process.env.DISCORD_FORGING_CHANNEL_WEBHOOK_URL,
     });
   }
 
@@ -24,19 +28,17 @@ export class DiscordService {
   }
 
   public async sendToChannel(event: ForgeEvent) {
-    if (event.eventTopic === EventTopics.forging) {
-      let content: string;
-      if (event.isSuccess) {
-        content = `${event.user} forged ${this.printElement(event.element)} with recipe ${this.printRecipe(event.recipe)}`;
-      } else {
-        content = `${event.user} tried a new recipe ${this.printRecipe(event.recipe)} and failed -.-`;
-      }
-      return this.forgingClient.send({ content });
-    }
-
     if (event.eventTopic === EventTopics.inventing) {
       const content = `${event.user} invented ${event.element}! The recipe was ${this.printRecipe(event.recipe)}`;
       return this.inventingClient.send({ content });
+    }
+    if (event.eventTopic === EventTopics.inventionAttempt) {
+      const content = `${event.user} tried a new recipe ${this.printRecipe(event.recipe)}`;
+      return this.inventionAttemptsClient.send({ content });
+    }
+    if (event.eventTopic === EventTopics.forging) {
+      const content = `${event.user} forged ${this.printElement(event.element)} with recipe ${this.printRecipe(event.recipe)}`;
+      return this.forgingClient.send({ content });
     }
 
     console.log(`Unexpected event topic '${event.eventTopic}'`);
